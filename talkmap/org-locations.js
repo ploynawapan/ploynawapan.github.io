cf98d0a712eadd289f6cdb9bc7927df2a445b89c
ploynawapan <85379202+ploynawapan@users.noreply.github.com>
2021-07-27 19:03:01 +0700

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,167 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "La Jolla, California",
+    32.8473,
+    -117.2742
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
+    "Los Angeles, California",
     34.0543942,
     -118.2439408
+  ],
+  [
+    "Brunswick, Maine",
+    43.914016,
+    -69.966996
+  ],
+  [
+    "Wolfeboro, New Hampshire",
+    43.585207,
+    -71.209480
+  ],
+  [
+    "Plymouth, New Hampshire",
+     43.757020,
+    -71.688153
+  ],
+  [
+    "Orlando, Florida",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Brattleboro, Vermont",
+    42.850915, 
+    -72.557868
+  ]
+  [
+    "Park City, Utah",
+     40.646061, 
+    -111.497971
+  ]
+  [
+    "Boston, Massachusetts",
+     42.361145, 
+    -71.057083
+  ]
+  [
+    "NYC, New York",
+     40.730610, 
+    -73.935242
+  ]
+  [
+    "Bali, Indonesia",
+     -8.409518, 
+    115.188919
+  ]
+  [
+    "Iloilo, Phillipines",
+     10.720321, 
+     122.562019
+  ]
+  [
+    "Ubon Ratchathani, Thailand",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Kuala Lumpur, Malaysia",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Hong Kong",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Tokyo, Japan",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Pekking, China",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Hoi An, Vietnam",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Savannakhet, Laos",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Nebidor, Myanmar",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Siam Rieb, Cambodia",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Singapore",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Taipei, Taiwan",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Seoul, South Korea",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Budapest, Hungary",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Milan, Italy",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Praque, Czech",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Lucerne, Switzerland",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Germany",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Paris, France",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Bulgaria",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Turkey",
+     28.538336, 
+    -81.379234
+  ]
+  [
+    "Athens, Greece",
+     28.538336, 
+    -81.379234
   ]
 ];
